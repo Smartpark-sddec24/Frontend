6de42bf84f992a46e165ca45e151b3610b9a1d32
Ethan Haberer <ehaberer@iastate.edu>
2024-09-20 10:47:17 -0500

    NavBar done. In index.js

--- app/Pages/Home.js
+++ app/Pages/Home.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import NavBar from '../NavBar';
+import App from '..';
 
 export default function Home() {
 
--- app/index.js
+++ app/index.js
@@ -1,9 +1,126 @@
-import NavBar from './NavBar';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import { CommonActions } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Text, BottomNavigation } from 'react-native-paper';
+import FA5 from 'react-native-vector-icons/FontAwesome5';
+import FA6 from 'react-native-vector-icons/FontAwesome6';
+import Home from './Pages/Home';
+//import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavBar/>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+      tabBar={({ navigation, state, descriptors, insets }) => (
+        <BottomNavigation.Bar
+          navigationState={state}
+         safeAreaInsets={insets}
+          onTabPress={({ route, preventDefault }) => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (event.defaultPrevented) {
+              preventDefault();
+            } else {
+             navigation.dispatch({
+                ...CommonActions.navigate(route.name, route.params),
+                target: state.key,
+              });
+            }
+          }}
+          renderIcon={({ route, focused, color }) => {
+            const { options } = descriptors[route.key];
+            if (options.tabBarIcon) {
+              return options.tabBarIcon({ focused, color, size: 24 });
+            }
+
+            return null;
+          }}
+          getLabelText={({ route }) => {
+            const { options } = descriptors[route.key];
+            const label =
+              options.tabBarLabel !== undefined
+                ? options.tabBarLabel
+                : options.title !== undefined
+                ? options.title
+                : route.title;
+
+            return label;
+          }}
+        />
+      )}
+    >
+      
+      { <Tab.Screen
+        name="Park"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: 'Park',
+          tabBarIcon: ({ color, size }) => {
+            return <FA5 name="map-marker-alt" size={size} color={color} />;
+          },
+        }}
+      />  }
+
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => {
+            return <FA6 name="map-location-dot" size={size} color={color} />;
+          },
+        }}
+      />
+
+
+     { <Tab.Screen
+        name="Reserve"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: 'Reserve',
+          tabBarIcon: ({ color, size }) => {
+            return <FA5 name="ticket-alt" size={size} color={color} />;
+          },
+        }}
+      />  }
+
+
+    </Tab.Navigator>
   );
 }
 
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text variant="headlineMedium">Home!</Text>
+    </View>
+  );
+}
 
+function SettingsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text variant="headlineMedium">Settings!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
