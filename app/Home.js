cb0cfa9c6faaa2fa21b9eaf735b9986e288af002
William Clemmons <wclemms@iastate.edu>
2024-09-20 12:41:31 -0500

    Map now centers on user location and updates region state

--- app/Home.js
+++ app/Home.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, View, Text } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function Home() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [region, setRegion] = useState(null)
+  useEffect(() => {
+    (async () => {
+      let {status} = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setOut(status)
+        setErrorMsg('Permission to access location was denied');
+        //return;
+      }
+      let location = await Location.getCurrentPositionAsync();
+      setLocation(location);
+      setRegion({
+        latitude: location["coords"]["latitude"],
+        longitude: location["coords"]["longitude"],
+        latitudeDelta: 1.0,
+        longitudeDelta: 1.0,
+      })
+    })();
+  }, []);
+
+  const onRegionChange = (newRegion) => {
+    setRegion(newRegion);
+  }
+
+  let text = <Text>"Waiting"</Text>;
+  if (errorMsg) {
+    text = <Text>{errorMsg}</Text>;
+  } else if (location) {
+    
+    text = <MapView style={styles.map} 
+    initialRegion={region}
+    Region={region}
+    onRegionChange={onRegionChange}
+    />;
+  } 
+
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      {text}
+      <Text>{region['longitude']}</Text>
     </View>
   );
 }
@@ -15,7 +56,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   map: {
-    width: '100%',
-    height: '100%',
+    width: '50%',
+    height: '50%',
   },
 });
