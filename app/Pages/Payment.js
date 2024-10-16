3e137a9b2ee4cef079b5b9dc44ad6e967f4f8bd4
Ethan Haberer <ehaberer@iastate.edu>
2024-10-16 15:11:02 -0500

    Will and Ethan Stripe session

--- app/Pages/Payment.js
+++ app/Pages/Payment.js
@@ -1,16 +1,38 @@
-import { Text, View } from "react-native"
-import {loadStripe} from '@stripe/stripe-js';
+import React, {useState, useEffect} from 'react';
+import {Elements, PaymentElement} from '@stripe/react-stripe-js';
 import { StripeProvider } from '@stripe/stripe-react-native';
-import React from 'react';
-import Stripe from '../Components/Stripe';
+import CheckoutScreen from '../Components/CheckoutScreen';
+import {loadStripe} from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(process.env.publishableKey);
+
 
 const Payment = () => {
+  const [publishableKey, setPublishableKey] = useState(process.env.publishableKey);
+
+  // const fetchPublishableKey = async () => {
+  //   const key = await fetchKey(); // fetch key from your server here
+  //   setPublishableKey(key);
+  // };
+
+  // useEffect(() => {
+  //   fetchPublishableKey();
+  // }, []);
+
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: process.env.secret,
+  };
+
   return (
     <StripeProvider
-      publishableKey="pk_test_51Q8RaWRuVD9MUnlbSNJuKodjS0Pc7LkGTZE4UziRlzXoAGnRtrZU0ijWSraxAejkvzNalYcdu8KJVAIJbZtmbz0D00iASGsqE3" // Replace with your actual publishable key
-    >
-      <Stripe/>
-    </StripeProvider>
+      publishableKey={publishableKey}
+      merchantIdentifier='anything'
+      >
+        <Elements stripe={stripePromise}>
+          <CheckoutScreen/>
+        </Elements>
+      </StripeProvider>
   );
 };
 
