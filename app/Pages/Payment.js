3631ef7c2a653e5fb5ba8833c694289c5a2d562c
Ethan Haberer <ehaberer@iastate.edu>
2024-10-23 18:12:38 -0500

    Added a text box for spot number

--- app/Pages/Payment.js
+++ app/Pages/Payment.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { StripeProvider } from '@stripe/stripe-react-native';
 import CheckoutScreen from '../Components/CheckoutScreen';
+import SpotNumber from '../Components/SpotNumber';
 
 const Payment = () => {
   const publishableKey = "pk_test_51Q8RaWRuVD9MUnlbSNJuKodjS0Pc7LkGTZE4UziRlzXoAGnRtrZU0ijWSraxAejkvzNalYcdu8KJVAIJbZtmbz0D00iASGsqE3"; // Replace with your actual Stripe publishable key
@@ -12,6 +13,7 @@ const Payment = () => {
       publishableKey={publishableKey}
       merchantIdentifier="anything" // Optional, for Apple Pay
     >
+        <SpotNumber />
       <CheckoutScreen />
     </StripeProvider>
   );
